5fb339fc7ab54a6a90968c6641b44e4a9bf44199
Yawar Amin <yawar.amin@gmail.com>
2023-12-03 22:46:37 -0500

    Day 2 part 2
    Move cubset type and functions into its own module since it now has a
    discrete set of operations.

--- d02/d02.ml
+++ d02/d02.ml
@@ -1,31 +1,40 @@
-type cubeset = { red : int; green : int; blue : int }
-type game = { id : int; cubesets : cubeset list }
+module Cubeset = struct
+  type t = { red : int; green : int; blue : int }
 
-let cubeset_add { red = r1; green = g1; blue = b1 }
-    { red = r2; green = g2; blue = b2 } =
-  { red = r1 + r2; green = g1 + g2; blue = b1 + b2 }
+  let add { red = r1; green = g1; blue = b1 }
+      { red = r2; green = g2; blue = b2 } =
+    { red = r1 + r2; green = g1 + g2; blue = b1 + b2 }
 
-let cubseset_leq { red = r1; green = g1; blue = b1 }
-    { red = r2; green = g2; blue = b2 } =
-  r1 <= r2 && g1 <= g2 && b1 <= b2
+  let parse_join initial str =
+    match String.split_on_char ' ' str with
+    | [ ""; num_str; clr ] ->
+        let num = int_of_string num_str in
+        let next =
+          match clr with
+          | "red" -> { red = num; green = 0; blue = 0 }
+          | "green" -> { red = 0; green = num; blue = 0 }
+          | "blue" -> { red = 0; green = 0; blue = num }
+          | _ -> assert false
+        in
+        add initial next
+    | _ -> assert false
 
-let cubeset_parse_join initial str =
-  match String.split_on_char ' ' str with
-  | [ ""; num_str; clr ] ->
-      let num = int_of_string num_str in
-      let next =
-        match clr with
-        | "red" -> { red = num; green = 0; blue = 0 }
-        | "green" -> { red = 0; green = num; blue = 0 }
-        | "blue" -> { red = 0; green = 0; blue = num }
-        | _ -> assert false
-      in
-      cubeset_add initial next
-  | _ -> assert false
+  let of_string str =
+    str |> String.split_on_char ','
+    |> List.fold_left parse_join { red = 0; green = 0; blue = 0 }
 
-let cubeset_of_string str =
-  str |> String.split_on_char ','
-  |> List.fold_left cubeset_parse_join { red = 0; green = 0; blue = 0 }
+  let leq { red = r1; green = g1; blue = b1 }
+      { red = r2; green = g2; blue = b2 } =
+    r1 <= r2 && g1 <= g2 && b1 <= b2
+
+  let max { red = r1; green = g1; blue = b1 }
+      { red = r2; green = g2; blue = b2 } =
+    { red = max r1 r2; green = max g1 g2; blue = max b1 b2 }
+
+  let power { red; green; blue } = red * green * blue
+end
+
+type game = { id : int; cubesets : Cubeset.t list }
 
 let game_of_string str =
   match String.split_on_char ':' str with
@@ -35,17 +44,17 @@ let game_of_string str =
             id;
             cubesets =
               cubesets_str |> String.split_on_char ';'
-              |> List.map cubeset_of_string;
+              |> List.map Cubeset.of_string;
           })
   | _ -> assert false
 
 let possible_id leq_cubeset game_str =
   let { id; cubesets } = game_of_string game_str in
-  if List.for_all (fun cubeset -> cubseset_leq cubeset leq_cubeset) cubesets
-  then id
+  if List.for_all (fun cubeset -> Cubeset.leq cubeset leq_cubeset) cubesets then
+    id
   else 0
 
-let real_cubeset = { red = 12; green = 13; blue = 14 }
+let real_cubeset = { Cubeset.red = 12; green = 13; blue = 14 }
 
 (* 2593 *)
 let part1 () =
@@ -53,4 +62,14 @@ let part1 () =
     (fun sum line -> sum + possible_id real_cubeset line)
     0
 
-let () = Printf.printf "Part 1: %d\n" (part1 ())
+let min_cubeset { cubesets; _ } =
+  match cubesets with
+  | cubeset :: cubesets -> List.fold_left Cubeset.max cubeset cubesets
+  | [] -> assert false
+
+let part2 () =
+  Lib.fold_file_lines "input"
+    (fun sum line -> sum + Cubeset.power (min_cubeset (game_of_string line)))
+    0
+
+let () = Printf.printf "Part 1: %d\nPart 2: %d\n" (part1 ()) (part2 ())
